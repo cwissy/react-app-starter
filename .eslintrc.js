127a387cb360034b5e3abc263f197f910f20d6e7
cwissy <meahchris@gmail.com>
2017-10-26 20:08:23 +0100

    changed folder structure and edited .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
     "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true,
+        "jest": true
+    },
     "rules": {
       // disable rules from base configurations
       "react/jsx-filename-extension": "off",
+      "import/no-unresolved": "off",
+      "import/extensions": "off"
     }
 };
