967735b338b8ac967ac81073e2369265497e0a86
cwissy <meahchris@gmail.com>
2017-10-26 12:38:42 +0100

    test a simple change for linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+      // disable rules from base configurations
+      "react/jsx-filename-extension": "off",
+    }
 };
